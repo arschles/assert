4888c48f6ff33491b94b70331c349a11ddcec154
Aaron Schlesinger <aschlesinger@paypal.com>
2014-11-17 16:08:11 -0800

    running go fmt

--- assert.go
+++ assert.go
@@ -9,67 +9,67 @@
 package assert
 
 import (
-  "testing"
-  "runtime"
-  "fmt"
+	"fmt"
+	"runtime"
+	"testing"
 )
 
 //callerStr returns a string representation of the code numFrames stack
 //frames above the code that called callerStr
 func callerStr(numFrames int) string {
-  _, file, line, _ := runtime.Caller(1 + numFrames)
-  return fmt.Sprintf("%s:%d", file, line)
+	_, file, line, _ := runtime.Caller(1 + numFrames)
+	return fmt.Sprintf("%s:%d", file, line)
 }
 
 //callerStrErrorf calls t.Errorf with fmtStr and vals in it, prefixed
 //by a callerStr representation of the code numFrames above the caller of
 //this function
 func callerStrf(numFrames int, fmtStr string, vals ...interface{}) string {
-  origStr := fmt.Sprintf(fmtStr, vals...)
-  return fmt.Sprintf("%s: %s", callerStr(1 + numFrames), origStr)
+	origStr := fmt.Sprintf(fmtStr, vals...)
+	return fmt.Sprintf("%s: %s", callerStr(1+numFrames), origStr)
 }
 
 //True calls t.Errorf if the provided bool is false, does nothing
 //otherwise
 func True(t *testing.T, b bool, fmtStr string, vals ...interface{}) {
-  if !b {
-    t.Errorf(callerStrf(1, fmtStr, vals...))
-  }
+	if !b {
+		t.Errorf(callerStrf(1, fmtStr, vals...))
+	}
 }
 
 //False is the equivalent of True(t, !b, fmtStr, vals...)
 func False(t *testing.T, b bool, fmtStr string, vals ...interface{}) {
-  if b {
-    t.Errorf(callerStrf(1, fmtStr, vals...))
-  }
+	if b {
+		t.Errorf(callerStrf(1, fmtStr, vals...))
+	}
 }
 
 //Nil calls t.Errorf if i is not nil
 func Nil(t *testing.T, i interface{}, fmtStr string, vals ...interface{}) {
-  if i != nil {
-    t.Errorf(callerStrf(1, fmtStr, vals...))
-  }
+	if i != nil {
+		t.Errorf(callerStrf(1, fmtStr, vals...))
+	}
 }
 
 //NoErr calls t.Errorf if e is not nil
 func NoErr(t *testing.T, e error) {
-  if e != nil {
-    t.Errorf(callerStrf(1, "expected no error but got %s", e))
-  }
+	if e != nil {
+		t.Errorf(callerStrf(1, "expected no error but got %s", e))
+	}
 }
 
 //Err calls t.Errorf if expected is not equal to actual
 func Err(t *testing.T, expected error, actual error) {
-  if expected != actual {
-    t.Errorf(callerStrf(1, "expected error %s but got %s", expected, actual))
-  }
+	if expected != actual {
+		t.Errorf(callerStrf(1, "expected error %s but got %s", expected, actual))
+	}
 }
 
 //Equal ensures that the actual value returned from a test was equal to an
 //expected. the last parameter is the name of the values that are being
 //compared. that parameter is used in the error string if actual != expected
 func Equal(t *testing.T, actual interface{}, expected interface{}, noun string) {
-  if actual != expected {
-    t.Errorf(callerStrf(1, "actual %s was [%+v], expected was [%+v]", noun, actual, expected))
-  }
+	if actual != expected {
+		t.Errorf(callerStrf(1, "actual %s was [%+v], expected was [%+v]", noun, actual, expected))
+	}
 }
